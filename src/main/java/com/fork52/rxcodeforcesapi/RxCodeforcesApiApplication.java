bc3a1204eadcd0b4f748097f123412e960d9d829
fork52 <mrunankmistry52@gmail.com>
2023-06-03 17:34:38 +0530

    Initial Commit

---
+++ src/main/java/com/fork52/rxcodeforcesapi/RxCodeforcesApiApplication.java
@@ -0,0 +1,13 @@
+package com.fork52.rxcodeforcesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxCodeforcesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RxCodeforcesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fork52/rxcodeforcesapi/RxCodeforcesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fork52.rxcodeforcesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RxCodeforcesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
