d3c89c0c0a1b59d4fc2a8885a7d5d2914630ae2b
fork52 <mrunankmistry52@gmail.com>
2023-07-02 21:58:06 +0530

    Add test for getContestStatus.

--- src/test/java/com/fork52/rxcodeforces/api/RxCodeforcesApiAuthenticatedTests.java
+++ src/test/java/com/fork52/rxcodeforces/api/RxCodeforcesApiAuthenticatedTests.java
@@ -4,14 +4,15 @@ import org.junit.jupiter.api.Test;
 import reactor.test.StepVerifier;
 
 class RxCodeforcesApiAuthenticatedTests {
-    private final String apiKey = System.getenv("CF_API_KEY");
-    private final String apiSecret = System.getenv("CF_API_SECRET");
 
-    @Test
-    void testUserFriends(){
-        StepVerifier.create(new CodeforcesWebClient(apiKey, apiSecret).getUserFriends(true))
-                .expectSubscription()
-                .expectNextCount(1)
-                .verifyComplete();
-    }
+  private final String apiKey = System.getenv("CF_API_KEY");
+  private final String apiSecret = System.getenv("CF_API_SECRET");
+
+  @Test
+  void testUserFriends() {
+    StepVerifier.create(new CodeforcesWebClient(apiKey, apiSecret).getUserFriends(true))
+        .expectSubscription()
+        .expectNextCount(1)
+        .verifyComplete();
+  }
 }
